4db47dd2de423ac63fcb4f3ab8285a7cd1bd3853
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-17 19:11:46 -0600

    Create method to test offset generation and create class method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -20,7 +20,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_get_random_five_digit_string
-    ("00000".."99999").to_a.include? (@enigma.generate_random_key_number)
+    assert ("00000".."99999").to_a.include? (@enigma.generate_random_key_number)
   end
 
   def test_get_keys
@@ -34,6 +34,17 @@ class EnigmaTest < Minitest::Test
                }
 
     assert_equal expected, @enigma.generate_keys
+  end
+
+  def test_get_offsets
+    expected = {
+                a: "1",
+                b: "0",
+                c: "2",
+                d: "5"
+               }
+
+    assert_equal expected, @enigma.generate_offsets("040895")
   end
 
 end
