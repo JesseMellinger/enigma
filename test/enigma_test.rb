f0891ff90ce3fa4acce73f05edea6dfe680f9c34
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-19 14:18:33 -0600

    Create method to test key hash creation

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,27 +12,8 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_get_character_set
-    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                 " "]
-
-    assert_equal expected, @enigma.character_set
-  end
-
   def test_get_random_five_digit_string
     assert ("00000".."99999").to_a.include? (@enigma.generate_random_key_number)
-  end
-
-  def test_get_keys
-    expected = {
-                a: "03",
-                b: "34",
-                c: "48",
-                d: "89"
-               }
-
-    assert_equal expected, @enigma.generate_keys("03489")
   end
 
   def test_get_offsets
