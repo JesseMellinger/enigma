fb0cb23349d7a8c4f1fc47ee80d4083ab196afbc
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-19 15:01:31 -0600

    Create Decryptor test class

---
+++ test/decryptor_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+require './lib/decryptor'
+
+class DecryptorTest < Minitest::Test
+
+  def setup
+    @decryptor = Decryptor.new()
+  end
+
+  def test_it_exists
+    assert_instance_of Decryptor, @decryptor
+  end
+
+end
