3be485393a86d435ed847560bbbd37dfcc96080a
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-19 14:11:13 -0600

    Create Encryptor test class

---
+++ test/encryptor_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+require './lib/encryptor'
+
+class EncryptorTest < Minitest::Test
+
+  def setup
+    @encryptor = Encryptor.new()
+  end
+
+  def test_it_exists
+    assert_instance_of Encryptor, @encryptor
+  end
+
+end
