b51d228e85d338717b379400451be3a10cae15ee
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-19 14:13:22 -0600

    Create reader method for attribute and test method

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -11,4 +11,12 @@ class EncryptorTest < Minitest::Test
     assert_instance_of Encryptor, @encryptor
   end
 
+  def test_get_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                 " "]
+
+    assert_equal expected, @encryptor.character_set
+  end
+
 end
